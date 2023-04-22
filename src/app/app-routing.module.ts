3ccc6f9c674747ea8dd733e5eec92e12afbf3d6c
m4v8r7s <79208623+m4v8r7s@users.noreply.github.com>
2023-04-22 11:49:13 +0530

    created with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ExperienceComponent } from './experience/experience.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'experience', component:ExperienceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { VideoBackgroundComponent } from './video-background/video-background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExperienceComponent,
+    VideoBackgroundComponent
   ],
   imports: [
     BrowserModule,
